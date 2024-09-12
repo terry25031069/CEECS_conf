22d88a39b2eede856653cc9ba57357b6e9ee820c
bluewhite <terry25031069@gmail.com>
2024-09-12 15:07:42 +0800

    update the information and fix some mirror issue

--- CSIT2017/app.py
+++ CSIT2017/app.py
@@ -4,7 +4,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    topics0 = ["電腦網路與資訊處理技術", "分散式系統與雲端計算", "資料庫與大數據科學", "人工智慧與機器學習", "多媒體影音處理", "數位學習與學習科技", "生物資訊與醫學工程"]
+    topics1 = ["射頻元件與積體電路設計",  "無線通訊與訊號處理", "無線、感測、行動網路與物聯網", "虛擬擴增實境與人機介面設計", "社群網路服務與大數據分析", "網路安全與區塊鏈技術", "軟體工程"]
+    return render_template('index.html', topics0=topics0, topics1=topics1)
 
 @app.route('/agenda')
 def agenda():
@@ -23,4 +25,5 @@ def program():
     return render_template('program.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+   app.run(debug=True)
+   # app.run(debug=False, host="140.115.59.23")
