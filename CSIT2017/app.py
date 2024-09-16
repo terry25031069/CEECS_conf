12052168fbe3534623a8296616ce5665f379957c
bluewhitesheen <terry25031069@gmail.com>
2024-09-17 01:37:25 +0800

    modify the agenda layout

--- CSIT2017/app.py
+++ CSIT2017/app.py
@@ -13,45 +13,41 @@ def agenda():
             "date": "11月27日(星期一)",
             "divname": "day1",
             "events": [
-                {"time": "17:00", "description": "抵達桃園國際機場", "colspan": 1, "type": "1"},
-                {"time": "18:30", "description": "註冊/接待晚宴/安排住宿", "colspan": 1, "type": "1"}
+                {"time": "17:00", "description": ["抵達桃園國際機場"], "colspan": 1, "type": "1"},
+                {"time": "18:30", "description": ["註冊/接待晚宴/安排住宿"], "colspan": 1, "type": "1"}
             ]
         },
         {
             "date": "11月28日(星期二)",
             "divname": "day2",
             "events": [
-                {"time": "09:00 ~ 09:30", "description": "開幕致詞", "colspan": 1, "type": "1"},
-                {"time": "09:30 ~ 09:45", "description": "貴賓合影", "colspan": 4, "type": "0"},
-                {"time": "09:45 ~ 10:30", "description": "【專題演講I】  演講者：中央大學資訊工程學系 張嘉惠 教授  講 題：From Information Extraction to Message Understanding - The Ultimate Challenge of AI 人工智慧的終極挑戰-自然語言理解  地 點：工程五館A207  主持人：國立中央大學資訊工程學系許富皓主任", "colspan": 4, "type": "1"},
-                {"time": "10:30 ~ 10:40", "description": "茶敘", "colspan": 4, "type": "0"},
-                {"time": "10:40 ~ 11:25", "description": "【專題演講II】  演講者：中央大學網路學習科技研究所 陳德懷 講座教授  講 題：明日閱讀與數位學習趨勢  地 點：工程五館A207  主持人：國立中央大學資電學院邱煥凱副院長", "colspan": 4, "type": "1"},
-                {"time": "11:25 ~ 12:10", "description": "【專題演講III】  演講者：南京大學計算機科學與技術系 高陽 教授  講 題：多智能體強化學習中的博弈與均衡  地 點：工程五館A207  主持人：東南大學電腦科學與工程學院李偉副院長", "colspan": 4, "type": "1"},
-                {"time": "12:10 ~ 13:30", "description": "午餐", "colspan": 4, "type": "0"},
-                {"time": "13:30 ~ 15:00", "description": "【研討會 A1】 通訊工程/電路設計  工程五館 A203", "colspan": 1, "type": "1"},
-                {"time": "13:30 ~ 15:00", "description": "【研討會 B1】 多媒體處理/資訊安全  工程五館 A301", "colspan": 1, "type": "1"},
-                {"time": "13:30 ~ 15:00", "description": "【研討會 C1】 醫療工程/深度學習  工程五館 A302", "colspan": 1, "type": "1"},
-                {"time": "15:00 ~ 15:30", "description": "茶敘", "colspan": 4, "type": "0"},
-                {"time": "15:30 ~ 17:00", "description": "【研討會 A2】 資料處理工程  工程五館 A203", "colspan": 1, "type": "1"},
-                {"time": "15:30 ~ 17:00", "description": "【研討會 B2】 資料傳輸工程  工程五館 A301", "colspan": 1, "type": "1"},
-                {"time": "15:30 ~ 17:00", "description": "【研討會 C2】 資料庫工程/學習科技   A302", "colspan": 1, "type": "1"},
-                {"time": "18:30 ~ 21:00", "description": "晚宴", "colspan": 1, "type": "1"}
+                {"time": "09:00 ~ 09:30", "description": ["開幕致詞"], "colspan": 4, "type": "1"},
+                {"time": "09:30 ~ 09:45", "description": ["貴賓合影"], "colspan": 4, "type": "0"},
+                {"time": "09:45 ~ 10:30", "description": ["【專題演講I】  演講者：中央大學資訊工程學系 張嘉惠 教授  講 題：From Information Extraction to Message Understanding - The Ultimate Challenge of AI 人工智慧的終極挑戰-自然語言理解  地 點：工程五館A207  主持人：國立中央大學資訊工程學系許富皓主任"], "colspan": 4, "type": "1"},
+                {"time": "10:30 ~ 10:40", "description": ["茶敘"], "colspan": 4, "type": "0"},
+                {"time": "10:40 ~ 11:25", "description": ["【專題演講II】  演講者：中央大學網路學習科技研究所 陳德懷 講座教授  講 題：明日閱讀與數位學習趨勢  地 點：工程五館A207  主持人：國立中央大學資電學院邱煥凱副院長"], "colspan": 4, "type": "1"},
+                {"time": "11:25 ~ 12:10", "description": ["【專題演講III】  演講者：南京大學計算機科學與技術系 高陽 教授  講 題：多智能體強化學習中的博弈與均衡  地 點：工程五館A207  主持人：東南大學電腦科學與工程學院李偉副院長"], "colspan": 4, "type": "1"},
+                {"time": "12:10 ~ 13:30", "description": ["午餐"], "colspan": 4, "type": "0"},
+                {"time": "13:30 ~ 15:00", "description": ["【研討會 A1】  通訊工程/電路設計  工程五館 A203", "【研討會 B1】  多媒體處理/資訊安全  工程五館 A301", "【研討會 C1】  醫療工程/深度學習  工程五館 A302"], "colspan": 1, "type": "1"},
+                {"time": "15:00 ~ 15:30", "description": ["茶敘"], "colspan": 4, "type": "0"},
+                {"time": "15:30 ~ 17:00", "description": ["【研討會 A2】  資料處理工程  工程五館 A203", "【研討會 B2】  資料傳輸工程  工程五館 A301", "【研討會 C2】  資料庫工程/學習科技   A302"], "colspan": 1, "type": "1"},
+                {"time": "18:30 ~ 21:00", "description": ["晚宴"], "colspan": 4, "type": "1"}
             ]
         },
         {
             "date": "11月29日(星期三)",
             "divname": "day3",
             "events": [
-                {"time": "09:00 ~ 12:00", "description": "技術會議(分組討論)  地點:工程五館A301、A203、A205會議室", "colspan": 1, "type": "1"},
-                {"time": "14:00 ~ 17:00", "description": "海報張貼  地點:工程五館一樓大廳", "colspan": 1, "type": "1"}
+                {"time": "09:00 ~ 12:00", "description": ["技術會議(分組討論)  地點:工程五館A301、A203、A205會議室"], "colspan": 1, "type": "1"},
+                {"time": "14:00 ~ 17:00", "description": ["海報張貼  地點:工程五館一樓大廳"], "colspan": 1, "type": "1"}
             ]
         },
         {
             "date": "11月30日(星期四)",
             "divname": "day4",
             "events": [
-                {"time": "09:00 ~ 12:00", "description": "技術會議(分組討論)  地點:工程五館A301、A203、A205會議室", "colspan": 1, "type": "1"},
-                {"time": "14:00 ~ 17:00", "description": "海報張貼  地點:工程五館一樓大廳", "colspan": 1, "type": "1"}
+                {"time": "09:00 ~ 12:00", "description": ["技術會議(分組討論)  地點:工程五館A301、A203、A205會議室"], "colspan": 1, "type": "1"},
+                {"time": "14:00 ~ 17:00", "description": ["海報張貼  地點:工程五館一樓大廳"], "colspan": 1, "type": "1"}
             ]
         }
     ]
@@ -67,8 +63,10 @@ def agenda():
         
         # traverse all descriptions, split by "  "
         for event in range(len(schedule[day]['events'])):
-            tmp_list = schedule[day]['events'][event]['description'].split("  ")
-            schedule[day]['events'][event]['description'] = tmp_list
+            descriptions = schedule[day]['events'][event]['description']
+            for i in range(len(descriptions)):
+                tmp_list = descriptions[i].split("  ")
+                schedule[day]['events'][event]['description'][i] = tmp_list
 
     return render_template('agenda.html', schedule=schedule)
 
