05afd7cbe8764b5640d0c9bf1f72311764463d10
bluewhitesheen <terry25031069@gmail.com>
2024-12-29 02:57:08 +0800

    update schedule

--- CSIT2024/app.py
+++ CSIT2024/app.py
@@ -9,55 +9,55 @@ def index():
 @app.route('/agenda')
 def agenda():
     schedule = [
+        # {
+        #     "date": "1月6日(星期一)",
+        #     "divname": "day1",
+        #     "events": [
+        #         {"time": "17:00", "description": ["抵達桃園國際機場"], "colspan": 1, "type": "1"},
+        #         {"time": "18:30", "description": ["註冊/接待晚宴/安排住宿"], "colspan": 1, "type": "1"}
+        #     ]
+        # },
         {
-            "date": "11月27日(星期一)",
-            "divname": "day1",
-            "events": [
-                {"time": "17:00", "description": ["抵達桃園國際機場"], "colspan": 1, "type": "1"},
-                {"time": "18:30", "description": ["註冊/接待晚宴/安排住宿"], "colspan": 1, "type": "1"}
-            ]
-        },
-        {
-            "date": "11月28日(星期二)",
+            "date": "1月7日(星期二)",
             "divname": "day2",
             "events": [
-                {"time": "09:00 ~ 09:30", "description": ["開幕致詞"], "colspan": 4, "type": "1"},
-                {"time": "09:30 ~ 09:45", "description": ["貴賓合影"], "colspan": 4, "type": "0"},
-                {"time": "09:45 ~ 10:30", "description": ["【專題演講I】  演講者：中央大學資訊工程學系 張嘉惠 教授  講 題：From Information Extraction to Message Understanding - The Ultimate Challenge of AI 人工智慧的終極挑戰-自然語言理解  地 點：工程五館A207  主持人：國立中央大學資訊工程學系許富皓主任"], "colspan": 4, "type": "1"},
-                {"time": "10:30 ~ 10:40", "description": ["茶敘"], "colspan": 4, "type": "0"},
-                {"time": "10:40 ~ 11:25", "description": ["【專題演講II】  演講者：中央大學網路學習科技研究所 陳德懷 講座教授  講 題：明日閱讀與數位學習趨勢  地 點：工程五館A207  主持人：國立中央大學資電學院邱煥凱副院長"], "colspan": 4, "type": "1"},
-                {"time": "11:25 ~ 12:10", "description": ["【專題演講III】  演講者：南京大學計算機科學與技術系 高陽 教授  講 題：多智能體強化學習中的博弈與均衡  地 點：工程五館A207  主持人：東南大學電腦科學與工程學院李偉副院長"], "colspan": 4, "type": "1"},
-                {"time": "12:10 ~ 13:30", "description": ["午餐"], "colspan": 4, "type": "0"},
-                {"time": "13:30 ~ 15:00", "description": ["【研討會 A1】  通訊工程/電路設計  工程五館 A203", "【研討會 B1】  多媒體處理/資訊安全  工程五館 A301", "【研討會 C1】  醫療工程/深度學習  工程五館 A302"], "colspan": 1, "type": "1"},
-                {"time": "15:00 ~ 15:30", "description": ["茶敘"], "colspan": 4, "type": "0"},
-                {"time": "15:30 ~ 17:00", "description": ["【研討會 A2】  資料處理工程  工程五館 A203", "【研討會 B2】  資料傳輸工程  工程五館 A301", "【研討會 C2】  資料庫工程/學習科技   A302"], "colspan": 1, "type": "1"},
-                {"time": "18:30 ~ 21:00", "description": ["晚宴"], "colspan": 4, "type": "1"}
+                {"time": "09:00~09:30", "description": ["教研大樓 TR A203  開幕式  貴賓介紹及貴賓致詞"], "colspan": 4, "type": "0"},
+                {"time": "09:30~09:45", "description": ["貴賓合影"], "colspan": 4, "type": "0"},
+                {"time": "09:45~10:45", "description": ["【專題演講I】演講者：過敏意教授/上海交通大學  【專題演講II】演講者：施國琛教授/中央大學"], "colspan": 4, "type": "1"},
+                {"time": "10:45~11:00", "description": ["休息/交流時間"], "colspan": 4, "type": "1"},
+                {"time": "11:00~12:00", "description": ["【專題演講III】演講者：王帥教授/東南大學  【專題演講IV】演講者：胡誌麟教授/中央大學"], "colspan": 4, "type": "1"},
+                {"time": "12:00~14:00", "description": ["午餐"], "colspan": 4, "type": "0"},
+                {"time": "14:00~15:30", "description": ["【分組論壇一】  網路計算與電子分論壇  (Network Computing and Electronics)(I)  電機 E1-233 會議室", "【分組論壇二】  人工智能分論壇  (Artificial Intelligence) (I)  電機 E1-225 會議室", "【分組論壇三】  永續應用分論壇  (Sustainable Applications)  (I)  通訊 E1-211 會議室"], "colspan": 1, "type": "1"},
+                {"time": "15:30~16:00", "description": ["休息/交流時間"], "colspan": 4, "type": "0"},
+                {"time": "16:00~17:30", "description": ["【分組論壇一】  網路計算與電子分論壇  (Network Computing and Electronics)(II)  電機 E1-233 會議室", "【分組論壇二】  人工智能分論壇  (Artificial Intelligence) (II)  電機 E1-225 會議室", "【分組論壇三】  永續應用分論壇  (Sustainable Applications)  (II)  通訊 E1-211 會議室"], "colspan": 1, "type": "1"},
+                {"time": "18:00", "description": ["晚宴"], "colspan": 4, "type": "0"}
             ]
         },
         {
-            "date": "11月29日(星期三)",
+            "date": "1月8日(星期三)",
             "divname": "day3",
             "events": [
-                {"time": "09:00 ~ 12:00", "description": ["技術會議(分組討論)  地點:工程五館A301、A203、A205會議室"], "colspan": 1, "type": "1"},
-                {"time": "14:00 ~ 17:00", "description": ["海報張貼  地點:工程五館一樓大廳"], "colspan": 1, "type": "1"}
+                {"time": "1/8", "description": ["海峽兩岸資訊技術研討會技術會議 跨校討論  國立清華大學、國立陽明交通大學"], "colspan": 1, "type": "2"}
             ]
         },
         {
-            "date": "11月30日(星期四)",
+            "date": "1月9日(星期四)",
             "divname": "day4",
             "events": [
-                {"time": "09:00 ~ 12:00", "description": ["技術會議(分組討論)  地點:工程五館A301、A203、A205會議室"], "colspan": 1, "type": "1"},
-                {"time": "14:00 ~ 17:00", "description": ["海報張貼  地點:工程五館一樓大廳"], "colspan": 1, "type": "1"}
+                {"time": "1/9", "description": ["海峽兩岸資訊技術研討會技術會議 跨校討論  國立中興大學"], "colspan": 1, "type": "2"}
             ]
         }
     ]
     for day in range(len(schedule)):
         for event in range(len(schedule[day]['events'])):
             if schedule[day]['events'][event]['type'] == "0":
-                schedule[day]['events'][event]['titlecolor'] = '#fff7d8'
-                schedule[day]['events'][event]['bgcolor'] = '#fff7d8'
+                schedule[day]['events'][event]['titlecolor'] = '#ffcccc'
+                schedule[day]['events'][event]['bgcolor'] = '#ffcccc'
             elif schedule[day]['events'][event]['type'] == "1":
-                schedule[day]['events'][event]['titlecolor'] = '#8ecaf5'
+                schedule[day]['events'][event]['titlecolor'] = '#ffffff'
+                schedule[day]['events'][event]['bgcolor'] = '#ffffff'
+            elif schedule[day]['events'][event]['type'] == "2":
+                schedule[day]['events'][event]['titlecolor'] = '#ffcccc'
                 schedule[day]['events'][event]['bgcolor'] = '#ffffff'
             else: pass
         
