d4d62426a9c5d8c0ab7e3a2e86888ad343551022
Terry Zheng <terry25031069@gmail.com>
2025-01-06 02:10:35 +0800

    update topies

--- CSIT2024/app.py
+++ CSIT2024/app.py
@@ -85,16 +85,17 @@ def agenda():
         }, {
             "title": "【分組論壇二】",
             "subtitle": "人工智能分論壇 (Artificial Intelligence) (I)",
-            "hosts": "曹付元教授、陳聿廣副教授",
+            "hosts": "吳小俊教授、孫敏德教授",
             "time": "14:00-15:30",
             "location": "電機 E1-225 會議室",
             "titlecolor": "8eaadb",
             "papers": [
-                {"id": "AI01", "title": "基於樣本穩定性的分類任務研究", "author": "曹付元"},
                 {"id": "AI02", "title": "帳單類別郵件之郵遞區號辨識", "author": "王文俊"},
                 {"id": "AI03", "title": "輸出可信的深度聚類網路", "author": "賈育衡"},
                 {"id": "AI04", "title": "AI 驅動的博物館文物識別：反光消除、影像處理與分類技術的集成應用", "author": "林家瑜"},
-                {"id": "AI05", "title": "表示學習研究進展與展望", "author": "梁吉業"}]
+                {"id": "AI06", "title": "CA-Wav2Lip: 基於坐標注意力的野外語音到唇部合成", "author": "孫敏德"},
+                {"id": "AI07", "title": "基於深度學習的多模態視覺融合方法", "author": "吳小俊"},
+                ]
         },{
             "title": "【分組論壇三】",
             "subtitle": "永續應用分論壇 (Sustainable Applications) (I)",
@@ -109,18 +110,16 @@ def agenda():
                 {"id": "SA04", "title": "以向日葵 8 號氣象衛星預測台灣區域雨量", "author": "陳映濃"},
                 {"id": "SA05", "title": "具有零電流開關和倍壓單元的高升壓直流-直流轉換器", "author": "徐國鎧"}]
         },{
-            "title": "【分組論壇一】",
+            "title": "【分組論壇一】(每篇發表時間為 10 分鐘)",
             "subtitle": "網路計算與電子分論壇 (Network Computing and Electronics) (II)",
-            "hosts": "王帥教授、陳永芳教授",
+            "hosts": "吳文甲教授、陳永芳教授",
             "time": "16:00~17:30",
             "location": "電機 E1-233 會議室",
             "titlecolor": "ffe599",
             "papers": [
                 {"id": "NCE06", "title": "矽量子點和光熱半導體奈米材料於綠能與生醫領域的應用", "author": "杜長慶"},
-                {"id": "NCE07", "title": "Wepos 面向即時配送的弱監督無標籤 Wi-Fi 室內定位", "author": "王帥"},
                 {"id": "NCE08", "title": "RFI 對基於 FIR 的自適應均衡器影響的研究", "author": "薛木添"},
                 {"id": "NCE09", "title": "基於 Zadoff-Chu 序列之多輸入多輸出低軌道衛星通訊系統迭代載波頻率偏移與通道估測演算法設計", "author": "陳永芳"},
-                {"id": "NCE10", "title": "28 奈米 2Mb 22.4 – 41.5TOPS/W 的 AI 推理性能 STT-MRAM 計算存儲器宏單元", "author": "蔡浩"},
                 {"id": "NCE11", "title": "運用高效能圖形處理器與 AI 技術於無人機之實時影像偵測與追蹤", "author": "張大中"}]
         }, {
             "title": "【分組論壇二】",
@@ -130,11 +129,11 @@ def agenda():
             "location": "電機 E1-225 會議室",
             "titlecolor": "ffe599",
             "papers": [
-                {"id": "AI06", "title": "CA-Wav2Lip: 基於坐標注意力的野外語音到唇部合成", "author": "孫敏德"},
-                {"id": "AI07", "title": "基於深度學習的多模態視覺融合方法", "author": "吳小俊"},
                 {"id": "AI08", "title": "雙曲表徵的龐加萊核", "author": "方鵬飛"},
                 {"id": "AI09", "title": "模組化虛擬助教與生成式 AI 學習平台之實證研究：以 Python 課程為例", "author": "張家凱"},
-                {"id": "AI10", "title": "考量老化的 AI 加速器設計", "author": "陳聿廣"}]
+                {"id": "AI10", "title": "考量老化的 AI 加速器設計", "author": "陳聿廣"},
+                {"id": "SA11", "title": "A Speech and Lip Reading System with Generating Text to Assist Hearing-Impaired People", "author": "蔡宗漢"}
+                ]
         },{
             "title": "【分組論壇三】",
             "subtitle": "永續應用分論壇 (Sustainable Applications) (II)",
@@ -148,7 +147,6 @@ def agenda():
                 {"id": "SA08", "title": "基於姿態辨識與壓力感測的智能瑜伽指導應用", "author": "蘇木春"},
                 {"id": "SA09", "title": "運用「數位魔鏡」情境學習模式提升職業教育學習成效與參與度：以餐旅管理系學生為例", "author": "陳國棟"},
                 {"id": "SA10", "title": "區塊鏈整合 V2X 網路之多連接管理效能評估", "author": "黃志煒"},
-                {"id": "SA11", "title": "A Speech and Lip Reading System with Generating Text to Assist Hearing-Impaired People", "author": "蔡宗漢"}
                 ]
         }
     ]
@@ -233,4 +231,4 @@ def program():
     return render_template('program.html')
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", debug=False, port=3333)
+    app.run(host="0.0.0.0", debug=True, port=3333)
