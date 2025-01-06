ebd4584e3fe8c175eda095fc928e87c5eea1f246
bluewhite <terry25031069@gmail.com>
2025-01-06 14:16:58 +0800

    update sched

--- CSIT2024/app.py
+++ CSIT2024/app.py
@@ -27,9 +27,10 @@ def agenda():
                 {"time": "10:45~11:00", "description": ["休息/交流時間"], "colspan": 4, "type": "1"},
                 {"time": "11:00~12:00", "description": ["【專題演講III】演講者：吴小俊教授/江南大学  【專題演講IV】演講者：陳正一教授/中央大學"], "colspan": 4, "type": "1"},
                 {"time": "12:00~14:00", "description": ["午餐"], "colspan": 4, "type": "0"},
-                {"time": "14:00~15:30", "description": ["【分組論壇一】  網路計算與電子分論壇  (Network Computing and Electronics)(I)  電機 E1-233 會議室", "【分組論壇二】  人工智能分論壇  (Artificial Intelligence) (I)  電機 E1-225 會議室", "【分組論壇三】  永續應用分論壇  (Sustainable Applications)  (I)  通訊 E1-211 會議室"], "colspan": 1, "type": "1"},
-                {"time": "15:30~16:00", "description": ["休息/交流時間"], "colspan": 4, "type": "0"},
-                {"time": "16:00~17:30", "description": ["【分組論壇一】  網路計算與電子分論壇  (Network Computing and Electronics)(II)  電機 E1-233 會議室", "【分組論壇二】  人工智能分論壇  (Artificial Intelligence) (II)  電機 E1-225 會議室", "【分組論壇三】  永續應用分論壇  (Sustainable Applications)  (II)  通訊 E1-211 會議室"], "colspan": 1, "type": "1"},
+                {"time": "14:00~15:00", "description": ["【分組論壇一】  網路計算與電子分論壇  (Network Computing and Electronics)(I)  電機 E1-233 會議室", "【分組論壇二】  人工智能分論壇  (Artificial Intelligence) (I)  電機 E1-225 會議室", "【分組論壇三】  永續應用分論壇  (Sustainable Applications)  (I)  通訊 E1-211 會議室"], "colspan": 1, "type": "1"},
+                {"time": "15:00~15:30", "description": ["休息/交流時間"], "colspan": 4, "type": "0"},
+                {"time": "15:30~16:20", "description": ["【分組論壇一】  網路計算與電子分論壇  (Network Computing and Electronics)(II)  電機 E1-233 會議室", "【分組論壇二】  人工智能分論壇  (Artificial Intelligence) (II)  電機 E1-225 會議室", "【分組論壇三】  永續應用分論壇  (Sustainable Applications)  (II)  通訊 E1-211 會議室"], "colspan": 1, "type": "1"},
+                {"time": "16:20~18:00", "description": ["能源屋參訪"], "colspan": 4, "type": "1"},
                 {"time": "18:00", "description": ["晚宴"], "colspan": 4, "type": "0"}
             ]
         },
@@ -231,4 +232,4 @@ def program():
     return render_template('program.html')
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", debug=False, port=3333)
+    app.run(host="0.0.0.0", debug=True, port=3333)
